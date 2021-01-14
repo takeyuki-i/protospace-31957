868255114c215915b47206ae9988b173b88efb92
takeyuki-i <golden_eggs7777@yahoo.co.jp>
2021-01-14 22:05:57 +0900

    commit 修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 before_action :devise_parameters, if: :devise_controller?
+before_action :authenticate_user!, only: [:new ,:create,:edit,:destroy]
 
 private
 def devise_parameters
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,4 @@
 class PrototypesController < ApplicationController
-before_action :authenticate_user!, only: [:create,:edit,:destroy]
 before_action :move_to_index, only: [:edit]
 
   def index
@@ -52,7 +51,8 @@ def prototypes_params
 end
 
 def move_to_index
-  unless user_signed_in?
+  @prototype = Prototype.find(params[:id])
+  unless user_signed_in? && current_user.id == @prototype.user_id
     redirect_to root_path
   end
 end
